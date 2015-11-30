76b9edff2b25e72ce534381d70d5b581b17f47d2
刘士涛 <acm_tach@163.com>
2015-11-30 12:30:55 +0800

    随机数生成器，支持跳变显示

---
+++ 随机数生成器/Form1.Designer.cs
@@ -0,0 +1,134 @@
+﻿namespace 随机数生成器
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.gb1 = new System.Windows.Forms.GroupBox();
+            this.lbl1 = new System.Windows.Forms.Label();
+            this.gb2 = new System.Windows.Forms.GroupBox();
+            this.lbl2 = new System.Windows.Forms.Label();
+            this.rTb = new System.Windows.Forms.RichTextBox();
+            this.btn = new System.Windows.Forms.Button();
+            this.gb1.SuspendLayout();
+            this.gb2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // gb1
+            // 
+            this.gb1.Controls.Add(this.lbl1);
+            this.gb1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.gb1.Location = new System.Drawing.Point(0, 0);
+            this.gb1.Name = "gb1";
+            this.gb1.Size = new System.Drawing.Size(477, 256);
+            this.gb1.TabIndex = 0;
+            this.gb1.TabStop = false;
+            this.gb1.Text = "抽中号码";
+            // 
+            // lbl1
+            // 
+            this.lbl1.Dock = System.Windows.Forms.DockStyle.Left;
+            this.lbl1.Font = new System.Drawing.Font("宋体", 72F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.lbl1.ForeColor = System.Drawing.Color.Red;
+            this.lbl1.Location = new System.Drawing.Point(3, 17);
+            this.lbl1.Name = "lbl1";
+            this.lbl1.Size = new System.Drawing.Size(271, 236);
+            this.lbl1.TabIndex = 0;
+            this.lbl1.Text = "label1";
+            this.lbl1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // gb2
+            // 
+            this.gb2.Controls.Add(this.btn);
+            this.gb2.Controls.Add(this.lbl2);
+            this.gb2.Controls.Add(this.rTb);
+            this.gb2.Dock = System.Windows.Forms.DockStyle.Right;
+            this.gb2.Location = new System.Drawing.Point(333, 0);
+            this.gb2.Name = "gb2";
+            this.gb2.Size = new System.Drawing.Size(144, 256);
+            this.gb2.TabIndex = 1;
+            this.gb2.TabStop = false;
+            this.gb2.Text = "总人数";
+            // 
+            // lbl2
+            // 
+            this.lbl2.Font = new System.Drawing.Font("宋体", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.lbl2.Location = new System.Drawing.Point(100, 25);
+            this.lbl2.Name = "lbl2";
+            this.lbl2.Size = new System.Drawing.Size(38, 32);
+            this.lbl2.TabIndex = 1;
+            this.lbl2.Text = "位";
+            // 
+            // rTb
+            // 
+            this.rTb.Font = new System.Drawing.Font("宋体", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.rTb.Location = new System.Drawing.Point(19, 20);
+            this.rTb.Name = "rTb";
+            this.rTb.Size = new System.Drawing.Size(75, 37);
+            this.rTb.TabIndex = 0;
+            this.rTb.Text = "";
+            // 
+            // btn
+            // 
+            this.btn.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.btn.Font = new System.Drawing.Font("宋体", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.btn.Location = new System.Drawing.Point(5, 123);
+            this.btn.Name = "btn";
+            this.btn.Size = new System.Drawing.Size(133, 127);
+            this.btn.TabIndex = 2;
+            this.btn.Text = "开始";
+            this.btn.UseVisualStyleBackColor = true;
+            this.btn.Click += new System.EventHandler(this.btn_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(477, 256);
+            this.Controls.Add(this.gb2);
+            this.Controls.Add(this.gb1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "随机数生成器";
+            this.gb1.ResumeLayout(false);
+            this.gb2.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox gb1;
+        private System.Windows.Forms.GroupBox gb2;
+        private System.Windows.Forms.Label lbl2;
+        private System.Windows.Forms.RichTextBox rTb;
+        private System.Windows.Forms.Button btn;
+        private System.Windows.Forms.Label lbl1;
+    }
+}
+
---
+++ 随机数生成器/Form1.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Threading;
+
+namespace 随机数生成器
+{
+    public partial class Form1 : Form
+    {
+        bool flag = false;
+        public Form1()
+        {
+            InitializeComponent();
+            lbl1.Text = "";
+        }
+        public void getRandnum()
+        {
+                Random rd = new Random();
+                int maxnum = int.Parse(rTb.Text);
+                int num = rd.Next(1, maxnum + 1);
+                lbl1.Text = num.ToString();
+                Delay(10);
+
+        }
+        private void Delay(int Millisecond) //延迟系统时间，但系统又能同时能执行其它任务；
+        {
+            DateTime current = DateTime.Now;
+            while (current.AddMilliseconds(Millisecond) > DateTime.Now)
+            {
+                Application.DoEvents();//转让控制权            
+            }
+            return;
+        }
+
+        private void btn_Click(object sender, EventArgs e)
+        {
+            if (rTb.Text == "")
+            {
+                MessageBox.Show("您还没有输入总的人数!", "警告", MessageBoxButtons.OKCancel, MessageBoxIcon.Error);
+                return;
+            }
+            int maxnum = int.Parse(rTb.Text);
+            flag = !flag;
+            if ((btn.Text).Equals("开始") == true)
+            {
+                btn.Text = "暂停";
+                while (flag == true)
+                {
+                    getRandnum();
+                }
+            }
+            if ((btn.Text).Equals("暂停") == true)
+              {
+                 btn.Text = "开始";
+              }
+
+        } 
+    }
+}
